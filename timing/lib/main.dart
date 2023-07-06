7f8f30c9e3a36ff61f22f78e525fd0d3bdf8c3dd
ragatt <ragatt@iu.edu>
2023-07-05 18:15:08 -0700

    Working view

--- timing/lib/main.dart
+++ timing/lib/main.dart
@@ -1,3 +1,7 @@
+
+
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,43 +14,45 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Timing',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(title: 'Timers'),
+      )
     );
   }
 }
 
+class MyAppState extends ChangeNotifier {
+  // List<TimerClock> timers = List.empty(growable: true);
+  List<TimerClock> timers = [];
+  
+  void timerStartStop(index) {
+
+    // if(timers[index].status == true) {
+    //   timers[index].stopTimer();
+    // } else {
+    //   timers[index].startTimer();
+    // }
+    print('Started/Stopped timer');
+    // notifyListeners();
+  }
+
+  void addTimer() {
+    timers.add(TimerClock());
+    print('Added timer');
+    notifyListeners();
+  }
+  //TO-DO: Add timer and Delete Timer functions
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -54,72 +60,173 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class Pair<T> {
+  late final T a;
+  late final T b;
+
+  Pair(this.a, this.b);
+}
+
+
+class TimerClock {
+  late DateTime start;
+  late DateTime end;
+  late Duration running;
+  bool status = false;
+  late String name;
+  late List<Pair<DateTime>> past;
+
+  TimerClock() {
+    running = Duration.zero;
+    past = [];
   }
+
+  void set(start, end, status, name) {
+    this.start = start;
+    this.end = end;
+    this.status = status;
+    running = Duration.zero;
+    past = [];
+    this.name = name;
+  }
+
+  void setName(name) {
+    this.name = name;
+  }
+
+  void startTimer() {
+    status = true;
+    start = DateTime.now();
+    print('Started timer');
+  }
+
+  void stopTimer() {
+    status = false;
+    end = DateTime.now();
+    print('Running before: $running');
+
+    running = running + end.difference(start);
+    past.add(Pair<DateTime>(start, end));
+    print('Stopped timer $running');
+  }
+  
+  void toggleTimer() {
+    if(status == true) {
+      stopTimer();
+    } else {
+      startTimer();
+    }
+  }
+}
+
+
+class _MyHomePageState extends State<MyHomePage> {
+
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
+    var appState = context.watch<MyAppState>();
+
+    var timersCount = appState.timers.length;
+
+    if(appState.timers.isEmpty) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {
+                appState.addTimer();
+                print('Timer added');
+              },
+              icon: const Icon(Icons.add_alarm_outlined))
+          ],
+        ),
+        body: const Center(
+          child: Text('No timers yet'),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              appState.addTimer();
+            },
+            icon: const Icon(Icons.add_alarm_outlined))
+        ],
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: <Widget> [
             const Text(
-              'You have pressed the button this many times:',
+              'Number of timers:',
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              '$timersCount'
+            ),  
+            Expanded(
+              child: ListView(
+                  children: <Widget>[
+                    for(final t in appState.timers) 
+                      TimerView(t: t),
+                  ],
+                ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class TimerView extends StatelessWidget {
+  TimerView({
+    super.key,
+    required this.t,
+  });
+
+  final TimerClock t;
+
+  final numFormat = NumberFormat('00', 'en_US');
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+  
+  final theme = Theme.of(context);
+  final timeStyle = theme.textTheme.displayMedium;//!.copyWith(color: theme.colorScheme.onSecondary);
+  // final cardStyle = theme.colorScheme.primary;
+  
+    return Center(
+      child: Card(
+        child: InkWell(
+          onLongPress: () => {
+            print('Long pressed this one')
+          },
+          onTap: () => { 
+            t.toggleTimer()
+          },
+          child: SizedBox(
+            height: 120,
+            width: 180,
+            child: Center(
+              child: Text(
+                '${numFormat.format(t.running.inMinutes)}:${numFormat.format(t.running.inSeconds%60)}',
+                style: timeStyle
+                ),
+            ),
+            ),
+        ),
+      ),
     );
   }
 }
